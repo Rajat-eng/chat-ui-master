7888cdc5f035bb7390486e1840bb9ef083c72015
Rajat <vrajat269@gmail.com>
2022-08-25 23:10:38 +0530

    Updates

---
+++ precache-manifest.379d8cc6b5df9691db1faed57411cbbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace1e0134270b6544e75eb85488bca60",
+    "url": "/chat-ui-master/index.html"
+  },
+  {
+    "revision": "5914f9048e2472bf3116",
+    "url": "/chat-ui-master/static/css/main.9911ccb4.chunk.css"
+  },
+  {
+    "revision": "1a7b35bedcdc96536640",
+    "url": "/chat-ui-master/static/js/2.fe5343f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat-ui-master/static/js/2.fe5343f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5914f9048e2472bf3116",
+    "url": "/chat-ui-master/static/js/main.2b1560da.chunk.js"
+  },
+  {
+    "revision": "a231ed94d55902083edc",
+    "url": "/chat-ui-master/static/js/runtime-main.01290ad5.js"
+  }
+]);
